dcb64ec089ec1cbac837535f799da23a6788bd04
cyberspouk <cyberspouk@gmail.com>
2018-07-31 09:41:42 +0300

    up

---
+++ flasher.go
@@ -0,0 +1,59 @@
+package goflash
+
+import (
+	"sync"
+	"time"
+	"fmt"
+	"crypto/md5"
+)
+
+type Flash struct {
+	sync.RWMutex
+	Key   string
+	Stock map[string]*FlashMessage
+	Salt  string
+}
+type FlashMessage struct {
+	Status  string
+	Message interface{}
+}
+
+//---------------------------------------------------------------------------
+//  FLASH:
+//---------------------------------------------------------------------------
+func newFlash(salt string ) *Flash {
+	n := &Flash{
+		Stock: make(map[string]*FlashMessage),
+		Salt: salt,
+	}
+	n.Key = n.generateKey()
+	return n
+}
+func (f *Flash) generateKey() string {
+	t := time.Now()
+	return fmt.Sprintf("%x", md5.Sum([]byte(t.String()+ f.Salt)))
+}
+func (f *Flash) Set(status, section string, message interface{}) {
+	nm := &FlashMessage{Status: status, Message: message}
+	f.Lock()
+	f.Stock[section] = nm
+	f.Unlock()
+}
+func (f *Flash) Get(section string) (*FlashMessage) {
+	f.Lock()
+	defer f.Unlock()
+	if result, exists := f.Stock[section]; exists {
+		delete(f.Stock, section)
+		return result
+	}
+	return nil
+}
+func (f *Flash) HaveMsg(section string) bool {
+	f.Lock()
+	defer f.Unlock()
+	_, exists := f.Stock[section]
+	if exists {
+		return true
+	}
+	return false
+}
